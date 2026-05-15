e47f595554884daad3ffc7b8f15c55097c2640f6
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-15 23:56:20 +0400

    Initialize PWA install prompt listener

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import { ErrorBoundary } from './app/ErrorBoundary';
+import { setupInstallPromptListener } from './features/pwa/installPrompt';
 import { registerServiceWorker } from './features/pwa/registerServiceWorker';
 import './styles/global.css';
 import './styles/themes.css';
 import './styles/accessibility.css';
+
+setupInstallPromptListener();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
