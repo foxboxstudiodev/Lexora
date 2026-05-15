4de2f8affef5464e2f10d51918b6221589f752db
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-15 23:27:54 +0400

    Add extra curated level seeds

---
+++ src/features/levels/extraWordSeeds.ts
@@ -0,0 +1,23 @@
+import { WordSeed } from './wordBanks';
+
+export const extraWordSeeds: WordSeed[] = [
+  { language: 'en', letters: ['M', 'A', 'R', 'K', 'E', 'T'], mainWords: ['MARKET', 'MAKER', 'TAKE'], bonusWords: ['TEAM', 'MEAT', 'RATE'], themeId: 'ancient-city' },
+  { language: 'en', letters: ['C', 'A', 'S', 'T', 'L', 'E'], mainWords: ['CASTLE', 'SCALE', 'LACE'], bonusWords: ['TALE', 'SEAT', 'LAST'], themeId: 'ancient-city' },
+  { language: 'en', letters: ['O', 'R', 'A', 'N', 'G', 'E'], mainWords: ['ORANGE', 'GROAN', 'ANGER'], bonusWords: ['GEAR', 'NEAR', 'RAGE'], themeId: 'sunny-coast' },
+  { language: 'en', letters: ['S', 'I', 'L', 'V', 'E', 'R'], mainWords: ['SILVER', 'LIVER', 'RISE'], bonusWords: ['EVIL', 'VILE', 'VEIL'], themeId: 'aurora-peak' },
+
+  { language: 'es', letters: ['C', 'A', 'S', 'A'], mainWords: ['CASA', 'SACA', 'ASA'], bonusWords: [], themeId: 'ancient-city' },
+  { language: 'es', letters: ['P', 'E', 'R', 'R', 'O'], mainWords: ['PERRO', 'PERO', 'REO'], bonusWords: ['POR'], themeId: 'mystic-forest' },
+  { language: 'es', letters: ['N', 'O', 'C', 'H', 'E'], mainWords: ['NOCHE', 'ECHO', 'CON'], bonusWords: ['ECO'], themeId: 'northern-lights' },
+  { language: 'es', letters: ['L', 'I', 'B', 'R', 'O'], mainWords: ['LIBRO', 'ROL', 'RIO'], bonusWords: [], themeId: 'crystal-lake' },
+
+  { language: 'ru', letters: ['Д', 'О', 'М', 'А'], mainWords: ['ДОМА', 'ДОМ', 'МОДА'], bonusWords: ['ОДА'], themeId: 'ancient-city' },
+  { language: 'ru', letters: ['П', 'О', 'Л', 'Е'], mainWords: ['ПОЛЕ', 'ПОЛ', 'ЕЛ'], bonusWords: [], themeId: 'dawn-garden' },
+  { language: 'ru', letters: ['В', 'О', 'Д', 'А'], mainWords: ['ВОДА', 'ДВА'], bonusWords: [], themeId: 'crystal-lake' },
+  { language: 'ru', letters: ['Н', 'О', 'Г', 'А'], mainWords: ['НОГА', 'ГОН'], bonusWords: [], themeId: 'mystic-forest' },
+
+  { language: 'tr', letters: ['A', 'T', 'E', 'Ş'], mainWords: ['ATEŞ', 'TAŞ', 'EŞ'], bonusWords: ['AT'], themeId: 'aurora-peak' },
+  { language: 'tr', letters: ['O', 'K', 'U', 'L'], mainWords: ['OKUL', 'KOL', 'KUL'], bonusWords: ['OK'], themeId: 'dawn-garden' },
+  { language: 'tr', letters: ['S', 'E', 'V', 'G', 'İ'], mainWords: ['SEVGİ', 'SEV', 'EV'], bonusWords: [], themeId: 'sakura-valley' },
+  { language: 'tr', letters: ['Y', 'A', 'Z', 'I'], mainWords: ['YAZI', 'YAZ', 'AY'], bonusWords: ['AZ'], themeId: 'sky-temple' },
+];
