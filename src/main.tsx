b40bd38b33eff004058ff820b8b1735a889aa8c7
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-15 22:01:55 +0400

    Wrap app with error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
+import { ErrorBoundary } from './app/ErrorBoundary';
 import { registerServiceWorker } from './features/pwa/registerServiceWorker';
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
 
