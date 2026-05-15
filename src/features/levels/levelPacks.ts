cb875db4d8af6cc38bf4e1b89e6375387a9379c4
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-15 23:28:33 +0400

    Include extra curated seeds in generated levels

--- src/features/levels/levelPacks.ts
+++ src/features/levels/levelPacks.ts
@@ -1,5 +1,6 @@
+import { extraWordSeeds } from './extraWordSeeds';
 import { createLevelsFromSeeds } from './levelFactory';
 import { Level } from './types';
 import { wordSeeds } from './wordBanks';
 
-export const starterLevels: Level[] = createLevelsFromSeeds(wordSeeds);
+export const starterLevels: Level[] = createLevelsFromSeeds([...wordSeeds, ...extraWordSeeds]);
