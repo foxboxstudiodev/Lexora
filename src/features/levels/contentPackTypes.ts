39e4402cf5d6ed0cc59d8df6f3588751f9a59ec9
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-16 22:46:34 +0400

    Add level content pack source types

---
+++ src/features/levels/contentPackTypes.ts
@@ -0,0 +1,31 @@
+import { LanguageCode } from '../i18n/languages';
+
+export type LevelSourceEntry = {
+  packLevelNumber: number;
+  words: string[];
+  bonusWords?: string[];
+  locationId: string;
+  seed?: string;
+};
+
+export type LanguageContentPack = {
+  language: LanguageCode;
+  targetLevelCount: number;
+  entries: LevelSourceEntry[];
+};
+
+export type ContentPackBuildStats = {
+  language: LanguageCode;
+  targetLevelCount: number;
+  sourceEntryCount: number;
+  completionRate: number;
+};
+
+export function getContentPackBuildStats(pack: LanguageContentPack): ContentPackBuildStats {
+  return {
+    language: pack.language,
+    targetLevelCount: pack.targetLevelCount,
+    sourceEntryCount: pack.entries.length,
+    completionRate: pack.targetLevelCount === 0 ? 0 : pack.entries.length / pack.targetLevelCount,
+  };
+}
