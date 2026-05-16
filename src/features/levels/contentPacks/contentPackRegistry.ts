99604ac64461da3023380372156082dd831be22a
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-17 00:40:49 +0400

    Register playable language content packs

--- src/features/levels/contentPacks/contentPackRegistry.ts
+++ src/features/levels/contentPacks/contentPackRegistry.ts
@@ -1,9 +1,15 @@
 import { LanguageCode } from '../../i18n/languages';
 import { LanguageContentPack } from '../contentPackTypes';
 import { enContentPack } from './enContentPack';
+import { esContentPack } from './esContentPack';
+import { ruContentPack } from './ruContentPack';
+import { trContentPack } from './trContentPack';
 
 export const contentPacks: Partial<Record<LanguageCode, LanguageContentPack>> = {
   en: enContentPack,
+  es: esContentPack,
+  ru: ruContentPack,
+  tr: trContentPack,
 };
 
 export function getContentPack(language: LanguageCode): LanguageContentPack | null {
