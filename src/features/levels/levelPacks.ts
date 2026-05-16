99072403de0690afc08c0bdf9ebfa15eb8738e49
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-16 23:44:13 +0400

    Load starter levels from content pack pipeline

--- src/features/levels/levelPacks.ts
+++ src/features/levels/levelPacks.ts
@@ -1,6 +1,12 @@
+import { buildRuntimeLevelsFromRegisteredContentPacks } from './contentPacks/runtimeContentLevels';
 import { extraWordSeeds } from './extraWordSeeds';
 import { createLevelsFromSeeds } from './levelFactory';
 import { Level } from './types';
 import { wordSeeds } from './wordBanks';
 
-export const starterLevels: Level[] = createLevelsFromSeeds([...wordSeeds, ...extraWordSeeds]);
+const contentBuild = buildRuntimeLevelsFromRegisteredContentPacks();
+const fallbackSeedLevels = createLevelsFromSeeds([...wordSeeds, ...extraWordSeeds]);
+
+export const contentPipelineIssues = contentBuild.issues;
+export const contentPipelineRejectedWords = contentBuild.rejectedWords;
+export const starterLevels: Level[] = contentBuild.levels.length > 0 ? contentBuild.levels : fallbackSeedLevels;
