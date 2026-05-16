4236d80d874bc7ffd344c9abc3c44c681f36b78e
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-16 23:55:57 +0400

    Add Turkish starter content pack

---
+++ src/features/levels/contentPacks/trContentPack.ts
@@ -0,0 +1,29 @@
+import { LanguageContentPack } from '../contentPackTypes';
+
+export const trContentPack: LanguageContentPack = {
+  language: 'tr',
+  targetLevelCount: 300,
+  entries: [
+    {
+      packLevelNumber: 1,
+      words: ['EV', 'DEV', 'VEDA'],
+      bonusWords: ['AD'],
+      locationId: 'eg-giza-pyramids',
+      seed: 'tr-pack-1',
+    },
+    {
+      packLevelNumber: 2,
+      words: ['YOL', 'OY', 'LOYAL'],
+      bonusWords: ['AL'],
+      locationId: 'fr-paris-eiffel',
+      seed: 'tr-pack-2',
+    },
+    {
+      packLevelNumber: 3,
+      words: ['KAPI', 'KIP', 'PAK'],
+      bonusWords: ['IP'],
+      locationId: 'az-baku-old-city',
+      seed: 'tr-pack-3',
+    },
+  ],
+};
