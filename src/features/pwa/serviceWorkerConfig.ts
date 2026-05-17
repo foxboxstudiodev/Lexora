56f6ac95ba7aced28cc884f535b03edcfa90e201
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-17 13:32:10 +0400

    Use root paths in service worker config

--- src/features/pwa/serviceWorkerConfig.ts
+++ src/features/pwa/serviceWorkerConfig.ts
@@ -1,10 +1,10 @@
 import { ServiceWorkerConfig } from './serviceWorkerAudit';
 
 export const serviceWorkerConfig: ServiceWorkerConfig = {
-  cacheName: 'lexora-cache-v2',
-  appScope: '/Lexora/',
-  appShell: ['/Lexora/', '/Lexora/manifest.webmanifest', '/Lexora/icon.svg'],
-  offlineFallback: '/Lexora/',
+  cacheName: 'lexora-cache-v3',
+  appScope: '/',
+  appShell: ['/', '/manifest.webmanifest', '/icon.svg'],
+  offlineFallback: '/',
   handlesOnlyGetRequests: true,
   restrictsToSameOrigin: true,
   cleansOldCaches: true,
