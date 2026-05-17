e39d23093c3945dcc752f3efa937855a3c2712a8
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-17 14:39:43 +0400

    Use root Vite base for app deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isVercel = process.env.VERCEL === '1';
-
 export default defineConfig({
   plugins: [react()],
-  base: isVercel ? '/' : '/Lexora/',
+  base: '/',
 });
