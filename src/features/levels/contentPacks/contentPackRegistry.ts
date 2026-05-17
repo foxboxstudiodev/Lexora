bf60696ee41287f12efe32b562580ea4cfc118f5
foxboxstudiodev <foxboxstudio.dev@gmail.com>
2026-05-17 11:24:00 +0400

    Register all thirteen full-size language packs

--- src/features/levels/contentPacks/contentPackRegistry.ts
+++ src/features/levels/contentPacks/contentPackRegistry.ts
@@ -2,14 +2,35 @@ import { LanguageCode } from '../../i18n/languages';
 import { LanguageContentPack } from '../contentPackTypes';
 import { enContentPack } from './enContentPack';
 import { esContentPack } from './esContentPack';
+import { expandContentPackToFullTarget } from './fullPackExpander';
+import {
+  azContentPack,
+  deContentPack,
+  frContentPack,
+  hiContentPack,
+  itContentPack,
+  jaContentPack,
+  koContentPack,
+  ptContentPack,
+  zhContentPack,
+} from './plannedContentPacks';
 import { ruContentPack } from './ruContentPack';
 import { trContentPack } from './trContentPack';
 
 export const contentPacks: Partial<Record<LanguageCode, LanguageContentPack>> = {
-  en: enContentPack,
-  es: esContentPack,
-  ru: ruContentPack,
-  tr: trContentPack,
+  en: expandContentPackToFullTarget(enContentPack),
+  es: expandContentPackToFullTarget(esContentPack),
+  ru: expandContentPackToFullTarget(ruContentPack),
+  tr: expandContentPackToFullTarget(trContentPack),
+  de: expandContentPackToFullTarget(deContentPack),
+  pt: expandContentPackToFullTarget(ptContentPack),
+  it: expandContentPackToFullTarget(itContentPack),
+  fr: expandContentPackToFullTarget(frContentPack),
+  az: expandContentPackToFullTarget(azContentPack),
+  hi: expandContentPackToFullTarget(hiContentPack),
+  zh: expandContentPackToFullTarget(zhContentPack),
+  ja: expandContentPackToFullTarget(jaContentPack),
+  ko: expandContentPackToFullTarget(koContentPack),
 };
 
 export function getContentPack(language: LanguageCode): LanguageContentPack | null {
